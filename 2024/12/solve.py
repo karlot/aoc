8fc874170e160286fdf28d69d97c450f805146e4
Karlo Tisaj <karlo.tisaj@axians.hr>
2024-12-27 10:37:37 +0100

    Suffering on day 12

--- 2024/12/solve.py
+++ 2024/12/solve.py
@@ -1,66 +1,11 @@
 import sys
 from collections import deque
-# from rich import print
 
+# 4 directions to move in a grid
 dir4 = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-
-def area(region):
-    return len(region)
-
-
-def perimeter(region):
-    p = 0
-    for x, y in region:
-        p += 4
-        for dx, dy in dir4:
-            if (x + dx, y + dy) in region:
-                p -= 1
-    return p
-
-def sides(region):
-    edges = {}
-    for x, y in region:
-        for dx, dy in dir4:
-            nx, ny = x + dx, y + dy
-            if (nx, ny) in region: continue
-            ey = (y + ny) / 2
-            ex = (x + nx) / 2
-            edges[(ex, ey)] = (ex - x, ey -y)
-    
-    seen = set()
-    side_count = 0
-    for edge, direction in edges.items():
-        if edge in seen:
-            continue
-
-        seen.add(edge)
-        side_count += 1
-
-        ex, ey = edge
-        if ex % 1 == 0:
-            for dy in [-1, 1]:
-                cy = ey + dy
-                while edges.get((ex, cy)) == direction:
-                    seen.add((ex, cy))
-                    cy += dy
-        else:
-            for dx in [-1, 1]:
-                cx = ex + dx
-                while edges.get((cx, ey)) == direction:
-                    seen.add((cx, ey))
-                    cx += dx
-
-    return side_count
-
-
-def solve(file):
-    with open(file, "r") as f:
-        content = f.read()
-    content = content.splitlines()
-
-    # Build grid layout
-    grid = [[c for c in row] for row in content]
+# Parses all regions in the grid, and returns them as a list of location sets
+def parse_regions(grid):
     gh = len(grid)      # grid height
     gw = len(grid[0])   # grid width
 
@@ -72,17 +17,16 @@ def solve(file):
             check_loc = (x, y)
             if check_loc in seen: continue
 
-            seen.add(check_loc)        # mark as seen
-            region = {check_loc}       # new region
-            queue = deque([check_loc]) # start BFS
-            plant = grid[y][x]      # current plant type
+            seen.add(check_loc)         # mark as seen
+            region = {check_loc}        # new region
+            queue = deque([check_loc])  # start BFS
 
             # Breadth First Search
             while queue:
                 cx, cy = queue.popleft()
                 for nx, ny in [(cx + dx, cy + dy) for dx, dy in dir4]:
-                    # Check if next position is out of bounds or not the same plant
-                    if nx < 0 or nx >= gw or ny < 0 or ny >= gh or grid[ny][nx] != plant:
+                    # Check if next position is out of bounds or not the same plant type that started the region
+                    if nx < 0 or nx >= gw or ny < 0 or ny >= gh or grid[ny][nx] != grid[y][x]:
                         continue
 
                     next_pos = (nx, ny)
@@ -91,15 +35,51 @@ def solve(file):
 
                     region.add(next_pos)
                     queue.append(next_pos)
-            
+
+            # Append all cells from region to seen set (union)
             seen |= region
             regions.append(region)
+
+    return regions
+
+
+# Area is basically lengths of positions in the region
+def area(region):
+    return len(region)
+
+
+# Perimeter is the sum of all 4 directions around a cell, minus the number
+# of cells that are adjacent to the current cell
+def perimeter(region):
+    perim = 0
+    for x, y in region:
+        perim += 4
+        for dx, dy in dir4:
+            if (x + dx, y + dy) in region:
+                perim -= 1
+    return perim
+
+
+# Function that calculates the number of edges of a region
+def edges(region):
+    # TODO: I dont know how to calculate this
+    ...
+
+
+def solve(file):
+    with open(file, "r") as f:
+        content = f.read()
+    content = content.splitlines()
+
+    # Build grid layout
+    grid = [[c for c in row] for row in content]
+    regions = parse_regions(grid)
 
     total_price = sum(area(r) * perimeter(r) for r in regions)
     print("Part 1:", total_price)
 
-    total_price2 = sum(len(r) * sides(r) for r in regions)
-    print("Part 2:", total_price2)
+    # total_price2 = sum(len(r) * sides(r) for r in regions)
+    # print("Part 2:", total_price2)
 
 
 if __name__ == "__main__":
